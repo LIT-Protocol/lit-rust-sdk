cf3351113e422fe6cd366432c341624c0e0c8de2
awisniew207 <andrew.wisniewski.08@gmail.com>
2025-12-31 10:49:39 -0800

    feat: add an optional capacity credit parameter to the local session signature generation (#16)
    * feat: add an optional capacity credit parameter to the local session signature generation
    
    * fix: change option to vector to match pkp implementation

--- lit-rust-sdk/src/client/session_sigs.rs
+++ lit-rust-sdk/src/client/session_sigs.rs
@@ -17,6 +17,7 @@ impl<P: alloy::providers::Provider> super::LitNodeClient<P> {
         wallet: &PrivateKeySigner,
         resource_ability_requests: Vec<LitResourceAbilityRequest>,
         expiration: &str,
+        capability_auth_sigs: Vec<AuthSig>,
     ) -> Result<SessionSignatures> {
         if !self.ready {
             return Err(eyre::eyre!("Lit Node Client not connected"));
@@ -49,7 +50,8 @@ impl<P: alloy::providers::Provider> super::LitNodeClient<P> {
             .sub(chrono::Duration::days(1))
             .to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
 
-        let capabilities = vec![auth_sig];
+        let mut capabilities = vec![auth_sig];
+        capabilities.extend(capability_auth_sigs);
 
         for node_url in self.connected_nodes() {
             let session_key_signed_message = SessionKeySignedMessage {
